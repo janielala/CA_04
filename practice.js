01184d98de281b59360052894c35ec9ea9c93026
janielala <148871742+janielala@users.noreply.github.com>
2023-11-11 02:34:43 +0000

    Initial commit

---
+++ practice.js
@@ -0,0 +1,49 @@
+// Your Javascript practice exercises go here
+// Take time to add your notes as comments here.
+
+/**
+ * Notes of JavaSript DataTypes and Variables
+ *
+ *
+ *
+ *
+ */
+// Practice making JavaScript Variables
+
+/**
+ * Notes of Conditional and Switch statements
+ *
+ *
+ *
+ */
+// Practice using conditional and swich statements
+
+/**
+ * Notes of JavaScript Loops
+ *
+ *
+ *
+ */
+// Practice trying out loops
+
+/**
+ * Notes of JavaScript Functions
+ *
+ *
+ *
+ *
+ *
+ */
+// Make Two JavaScript Functions below - One should include optional arguments and one should be an arrow function
+
+/**
+ * Some Questions to answer
+ *
+ * What is automatic type conversion?
+ *
+ * What is the importance of capitalisation in JavaScript?
+ *
+ * In your own words and with examples describe what function scope is?
+ *
+ * Create a function that contains closure, leave a comment describing what it does.
+ */
