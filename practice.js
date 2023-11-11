47d299c0bb4a6bd1c77dd3dab0d7089c1c7cba5f
janielala <janielaudesign@gmail.com>
2023-11-11 16:14:27 +0800

    1111update

--- practice.js
+++ practice.js
@@ -3,17 +3,26 @@
 
 /**
  * Notes of JavaSript DataTypes and Variables
- *
- *
- *
+ * DataTypes : 
+ * undefine
+ * null
+ * Boolean - true/false
+ * number
+ * string
+ * 
+ * Variable :
+ * var, const & let
+ * 
  *
  */
 // Practice making JavaScript Variables
-
+let a = 0;
+const b = 1;
+a=5
+// this will make an error b=20
+console.log(a, b);
 /**
  * Notes of Conditional and Switch statements
- *
- *
  *
  */
 // Practice using conditional and swich statements
@@ -21,7 +30,19 @@
 /**
  * Notes of JavaScript Loops
  *
- *
+ * IF Statement
+ * if (){}...else if (){}... else {}
+ *  
+ * DO...WHILE Statement
+ * do {} while ()
+ * Do it once then check the statement
+ * 
+ * For Loop
+ * for (i=1; i<10; i++) {}
+ * 
+ * Try 
+ * help ignore the error
+ * try {}
  *
  */
 // Practice trying out loops
@@ -29,10 +50,8 @@
 /**
  * Notes of JavaScript Functions
  *
- *
- *
- *
- *
+ * function() must goes with () to show the input variables
+ * 
  */
 // Make Two JavaScript Functions below - One should include optional arguments and one should be an arrow function
 
@@ -40,10 +59,12 @@
  * Some Questions to answer
  *
  * What is automatic type conversion?
- *
+ * JavaScript variables can be converted to a new variable and another data type by JavaScript itself
+ * 
  * What is the importance of capitalisation in JavaScript?
- *
+ * JavaScript is case sensitive, the function will be different when using upper case and lower case, it may occur error if the letter is not in a correct way
+ * 
  * In your own words and with examples describe what function scope is?
- *
+ * This means the variable is use only inside the function but not outside.
  * Create a function that contains closure, leave a comment describing what it does.
  */
